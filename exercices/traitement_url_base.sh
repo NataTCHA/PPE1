5ccebd64aaf9c1c845f85550e76fc4c947fbc789
NataTCHA <nminiconi@hotmail.fr>
2022-11-09 14:50:54 +0100

    nvx

--- exercices/traitement_url_base.sh
+++ exercices/traitement_url_base.sh
@@ -16,18 +16,31 @@ fi
 
 fichier_urls=$1 # le fichier d'URL en entrée
 fichier_tableau=$2 # le fichier HTML en sortie
-
 # !!!!!!
 # ici on doit vérifier que nos deux paramètres existent, sinon on ferme!
 # !!!!!!
 
 # modifier la ligne suivante pour créer effectivement du HTML
-echo "Je dois devenir du code HTML à partir de la question 3" > $fichier_tableau
-
+echo "<html>
+        <head>
+                <meta charset ="utf-8"/>
+                <title> tableau </title>
+                </header>
+                <body>
+                <table>
+                <tr>
+                <th> URLS </th>
+                <th>N°</th>
+                <th> codehttp</th> 
+                </tr></html>"
+                 
 lineno=1;
 
 while read -r line;
 do
 	echo "ligne $lineno: $line";
 	lineno=$((lineno+1));
+        header=$(curl -I -s $line | head -n1)	
+        echo "l'entête est  $header";
 done < $fichier_urls
+
